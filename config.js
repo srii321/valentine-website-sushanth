4e514fe483176f80c0b4092a9b5311c0295c0718
srii321 <77708952+srii321@users.noreply.github.com>
2026-01-28 20:29:46 +1000

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/djmp5dlzi/video/upload/v1769596062/Narivetta_-_Minnalvala_Lyrical_Tovino_Thomas_Anuraj_Manohar_Jakes_Bejoy_Sid_Sriram_Sithara_ftdkiv.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
